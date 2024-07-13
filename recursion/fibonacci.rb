37a69365232d240dad4a1f9e9dd4d47f43d1d026
Benjamin Jackson <benj@betterrentals.com.au>
2024-07-13 13:35:51 +1000

    completed fibonacci & recursive

--- recursion/fibonacci.rb
+++ recursion/fibonacci.rb
@@ -1,2 +1,12 @@
-def fibonacci
+def fibonacci(number)
+  array = []
+  (number + 1).times do |i|
+    i < 2 ? array << i : array << (array[i - 1]) + (array[i - 2])
+  end
+  array
+end
+
+def fibonacci_recursive(number, i = 0, array = [])
+  i < 2 ? array << i : array << (array[i - 1]) + (array[i - 2])
+  number.positive? ? fibonacci_recursive(number - 1, i + 1, array) : array
 end
