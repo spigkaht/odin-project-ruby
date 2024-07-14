9ec023f0cee0c248c1759b3580e8721f61c73b7c
Benjamin Jackson <benj@betterrentals.com.au>
2024-07-14 20:19:34 +1000

    odin

---
+++ hash-map/lib/hash_map.rb
@@ -0,0 +1,10 @@
+class HashMap
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+
+    hash_code
+  end
+end
---
+++ hash-map/main.rb
@@ -0,0 +1,5 @@
+require_relative "lib/hash_map"
+
+hash_map = HashMap.new
+
+p hash_map.hash("benjamin")
